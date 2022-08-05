cf6daa2ad83e24b27be31b31e42e906c69e75e80
igorjonny <igorjonny1@gmail.com>
2022-08-05 12:19:22 -0300

    fim do modulo

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,34 +6,33 @@ import {
   FlatList,
   Alert,
 } from "react-native";
+import { useState } from "react";
+
 import { Participant } from "../../components/Participant";
+
 import { styles } from "./styles";
 
 export function Home() {
-  const participants = [
-    "igor",
-    "jonny",
-    "cirino",
-    "de",
-    "matos",
-    "geni",
-    "jair",
-    "pereira",
-    "tais",
-    "thays",
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
 
   function handleParticipantAdd() {
-    if (participants.includes("igor")) {
+    if (participants.includes(participantName)) {
       return Alert.alert("participante existe", "ja existe um participante");
     }
+
+    setParticipants((oldState) => [...oldState, participantName]);
+    setParticipantName("");
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert("remover", `deseja remover ${name}`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("deletado"),
+        onPress: () =>
+          setParticipants((oldState) =>
+            oldState.filter((participant) => participant !== name)
+          ),
       },
       {
         text: "Não",
@@ -45,17 +44,25 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+      <Text style={styles.eventDate}>
+        <Text>Sexta, 4 de Novembro de 2022</Text>
+      </Text>
+
       <View style={styles.form}>
         <TextInput
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
+
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
       <FlatList
         data={participants}
         keyExtractor={(item) => item}
